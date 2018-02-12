cea620781d12d600990619fc96e82f97e5d28c88
adunietz <anna.dunietz@gmail.com>
2018-02-12 17:32:26 +0100

    loading data, initial descriptive statistics RMarkdown file (HTML)

--- 1_DataAnalysis/data_analysis.R
+++ 1_DataAnalysis/data_analysis.R
@@ -4,5 +4,14 @@
 library(RCurl)
 
 # Load data
-train_rawdata = read.csv(text = getURL("https://raw.githubusercontent.com/adunietz/airbnbprices/master/0_DataAndInitialization/train.csv"), header = TRUE)
-test_rawdata = read.csv(text = getURL("https://raw.githubusercontent.com/adunietz/airbnbprices/master/0_DataAndInitialization/test.csv"), header = TRUE)
+# Better to use RData files, as they're less heavy
+# train_rawdata = read.csv(text = getURL("https://raw.githubusercontent.com/adunietz/airbnbprices/master/0_DataAndInitialization/train.csv"), header = TRUE)
+# test_rawdata = read.csv(text = getURL("https://raw.githubusercontent.com/adunietz/airbnbprices/master/0_DataAndInitialization/test.csv"), header = TRUE)
+
+# Save to RData, so you can use in Rmarkdown
+# save(train_rawdata, file = "train_rawdata.RData")
+# save(test_rawdata, file = "test_rawdata.RData")
+
+# RData files from Github
+load(url("https://github.com/adunietz/airbnbprices/raw/master/0_DataAndInitialization/train_rawdata.RData"))
+load(url("https://github.com/adunietz/airbnbprices/raw/master/0_DataAndInitialization/test_rawdata.RData"))
